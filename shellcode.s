9e6e21ca467f924c9e40b030e0d5fed143d4f1eb
Graham Markall <graham.markall@embecosm.com>
2018-08-13 17:41:30 +0100

    Better comment on shellcode

--- shellcode.s
+++ shellcode.s
@@ -1,9 +1,11 @@
 func:
+        # lcd.setCursor(a0=this, a1=0, a2=0)
         li a2, 0
         li a1, 0
         li a0, 0x80000444
         li a5, 0x20401E08
         jalr ra, a5
+        # lcd.print(a0=this, a1=string to print)
         li a0, 0x80000444
         li a1, 0x80003fe4 # Address of pwstr - would be better if I figured out how to use a relocation!
         li a5, 0x20402c62
